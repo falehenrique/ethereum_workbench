97cb4f8bde6f8493d1fbb2b5ad3958ab9bebf37c
Henrique <fale.henrique@gmail.com>
2018-08-10 20:20:43 -0300

    Compra de tokens

--- goblockchain_wb/app/sol/CrowndFund.sol
+++ goblockchain_wb/app/sol/CrowndFund.sol
@@ -1,7 +1,21 @@
 pragma solidity ^0.4.24;
+
 import "./Ownable.sol";
 
-contract CrowndFund is Ownable {
+contract CrownFund is Ownable {
+    
+    event tokenComprado(address comprador); 
+    
+    function() public payable {
+        emit tokenComprado(msg.sender);
+    }
 
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+    
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
     
 }
--- goblockchain_wb/app/sol/TokenClass.sol
+++ goblockchain_wb/app/sol/TokenClass.sol
@@ -1,19 +1,33 @@
-pragma solidity ^0.4.24;
-
-import "./StandardToken.sol";
-
 contract TokenClass is StandardToken {
-    string public constant name = "TokenDApp";
+
+    string public constant name = "TokenClass";
     string public constant symbol = "CLS";
     uint8 public constant decimals = 18;
     
-    uint256 public constant INITIAL_SUPPLY = 100 * (10 ** uint256(decimals));
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
     
-    // event tokenBought(address adr);
+    event tokenBought(address adr);
     
-    constructor() public {
+    CrownFund public crownFund;
+    
+    uint256 public tokenPrice;
+    
+    constructor(uint256 _tokenPrice) public {
         totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
         emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+        
+        crownFund = new CrownFund();
+        tokenPrice = _tokenPrice;
     }
-}
+    
+    function comprarTokens(uint8 _qtd) public payable {
+        require(_qtd > 0);
+        require(msg.value > 0);
+        require(msg.value == (_qtd * tokenPrice));
+        
+        balances[owner] = balances[owner].sub(_qtd);
+        balances[msg.sender] = balances[msg.sender].add(_qtd);
+        address(crownFund).transfer(msg.value);
+        emit Transfer(owner, msg.sender, _qtd);
+    }
