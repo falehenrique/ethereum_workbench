7cc5b1bc56c594a1233cf6ebabb01abbdc757d1e
Henrique <fale.henrique@gmail.com>
2018-08-10 11:05:19 -0300

    Evento

--- goblockchain_wb/app/sol/Person.sol
+++ goblockchain_wb/app/sol/Person.sol
@@ -5,5 +5,43 @@ import "./CrowndFund.sol";
 import "./TokenClass.sol";
 
 contract Person is TokenClass {
+    address owner;
 
+    //date of create
+    uint public dateCreate;
+    uint public identificationPerson;
+
+    //name of person
+    string public name;
+
+    //variables to be validates
+    string public email;
+
+    //events
+    event LogUpdateEmail(address _addressPerson, string _newEmail); 
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // constructor
+    constructor (string _name, string _email) public {
+        name = _name;
+        email = _email;
+        owner = msg.sender;
+        dateCreate = now;
+        identificationPerson = block.number+now;
+        owner = msg.sender;
+    }
+
+    // change the email
+    function changeEmail(string _newMail) public onlyOwner {
+        email = _newMail;
+        emit LogUpdateEmail(this, email);
+    }
+    
+    function buyTokens(uint8 _qtd) public payable {
+
+    }
 }
